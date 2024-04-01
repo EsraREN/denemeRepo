50809450522d21c3084d53d544f0549dbd87fbef
EsraREN <119942465+EsraREN@users.noreply.github.com>
2024-04-01 09:58:37 +0300

    Atm.Java

--- 2sayfa.java
+++ 2sayfa.java
@@ -1,33 +1,28 @@
-package main.java;
+package pkt;
 
-import main.java.pc.abstracts.IComputer;
-import main.java.utility.impl.ComputerFactory;
-
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Scanner;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.print("Enter Path: ");
-        Scanner scanner = new Scanner(System.in);
-
-        boolean isValid;
-        String path;
-        do{
-            path = scanner.nextLine();
-            Path enteredPath= FileSystems.getDefault().getPath(path);
-            isValid= Files.exists(enteredPath);
-            if(!isValid)
-                System.out.print("Invalid Path!\nEnter Path: ");
-        }while(!isValid);
-
-        IComputer computer=new ComputerFactory().createComputer();
-        IUser user=new User(computer,path);
-        user.startComputer();
-
-    }
-
+/**
+ * 
+ * @author MFA
+ *
+ */
+public class Atm {
+	/**
+	 * <p>
+	 *   Atm'den para çekme işlemi
+	 * </p>
+	 * @param kart ATM'ye verilen kart
+	 * @param sifre Kart şifresi
+	 * @param miktar Çekilecek para miktarı
+	 * @return Para çekme işleminin başarılımı geçtiğini döndürür.
+	 */
+	public boolean paraCek(IKart kart,String sifre, double miktar) {
+		if(!kart.girisKontrol(sifre)) return false;
+		return kart.getHesap().paraCek(miktar);
+	}
+	
+	public boolean paraYatir(IKart kart,String sifre, double miktar) {
+		// Şifre kontrolü
+		if(!kart.girisKontrol(sifre)) return false;
+		return kart.getHesap().paraYatir(miktar);
+	}
 }
